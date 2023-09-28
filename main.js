76024ed66590bb552da18ec6e062cc52a64dbfb8
Junglepanda69 <Conrady82@gmail.com>
2023-09-28 15:17:59 -0700

    attempting rotation

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// Chrome extenstion that has a timer to force user to stretch
-// Once the timer has reached its set time to invoke, screen will blur via html
-// once screen is blurred, settime to remove blur for 5 min
-// timer needs to start when an event happens (event listener), initialize at page load
-// ------------create a cache variable to store the initialized time
-// set timeout for 1 hour to then inovoke blur
-// -------create recursive function that checks the time of cache and if the ---------time matches again,  invoke screen blur
-// create pop up with count down that shows how long of a duration to stretch for
-// use phils head in alert box
-// once time has passed, allow box to be closed
-
-// function invokeBlur(){
-//     //html blur
-// }
-// const blurPageEveryHour = setTimeout(invokeBlur,3600000)
 
 let startingMinutes = 5
 let time = startingMinutes * 60
@@ -25,6 +10,8 @@ function updateCountdown(){
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
+    if (minutes < 1 && seconds < 1) endStretch()
+
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countdownEl.innerHTML = `${minutes} : ${seconds}`;
@@ -35,6 +22,9 @@ function myFunction(){
     alert("Time For Stretchies!!!");
 }
 
+function endStretch(){
+    alerty("GET BACK TO CODING AND SITTING ALL FUNNY")
+}
 
 const type = 'stretching';
 const apiKey = 'q68lLZCB0j+666FyU3BXkw==FVWU07FF1UiNBUGP';
