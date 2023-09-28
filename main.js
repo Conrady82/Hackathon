4e0f916ecbc473004dacfd048995c78e01a8e5bc
Junglepanda69 <Conrady82@gmail.com>
2023-09-27 21:41:47 -0700

    what the hell why not

--- main.js
+++ main.js
@@ -35,11 +35,16 @@ function clearMessage() {
 const startingMinutes = 60
 let time = StartingMinutes * 60
 
+const coundownEl = document.getElementById('countdown')
+
 setInterval(updateCountdown, 1000);
 
 function updateCountdown(){
     const minutes= Math.floor(time/60);
     let seconds = time % 60;
 
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
     countdownEl.innerHTML = `${minutes} : ${seconds}`;
+    time--;
 }
