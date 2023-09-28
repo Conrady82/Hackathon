8768dc34b719e8b93d590f762de51bc842048f1e
Junglepanda69 <Conrady82@gmail.com>
2023-09-28 15:40:31 -0700

    timing done

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 
-let startingMinutes = 5
+let startingMinutes = .1
 let time = startingMinutes * 60
 
 let countdownEl = document.getElementById("countdown")
 
-setInterval(() => {updateCountdown()}, 1000)
+const intervalID = setInterval(() => {updateCountdown()}, 1000)
 
 function updateCountdown(){
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
-
-    if (minutes < 1 && seconds < 1) endStretch()
+    if (minutes < 1 && seconds === 0) {
+        endStretch()
+        clearInterval(intervalID)
+    };
 
     seconds = seconds < 10 ? '0' + seconds : seconds;
-
+    
     countdownEl.innerHTML = `${minutes} : ${seconds}`;
     time--;
 }
@@ -23,7 +25,7 @@ function myFunction(){
 }
 
 function endStretch(){
-    alerty("GET BACK TO CODING AND SITTING ALL FUNNY")
+    alert("GET BACK TO CODING AND SITTING ALL FUNNY")
 }
 
 const type = 'stretching';
